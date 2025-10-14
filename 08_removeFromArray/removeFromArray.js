06c87c0c1c8f5b23e275b3b028d07ba21fe4a6e6
bringesh-konda <119878875+bringesh-konda@users.noreply.github.com>
2025-10-14 18:11:44 +0530

    complete 08 removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    for(num of arg) {
+        let ind = arr.indexOf(num);
+        while(ind != -1) {
+            arr.splice(ind, 1);
+            ind = arr.indexOf(num);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
