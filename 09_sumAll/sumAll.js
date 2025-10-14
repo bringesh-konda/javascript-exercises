befd3a0c00db4404da52dfd3f2e7aa51c1cc761a
bringesh-konda <119878875+bringesh-konda@users.noreply.github.com>
2025-10-14 18:20:35 +0530

    complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0 ||  typeof a !== 'number' || typeof b !== 'number' || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
 
+    let sumi = 0;
+    for(let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sumi += i;
+    }
+    return sumi;
 };
 
 // Do not edit below this line
