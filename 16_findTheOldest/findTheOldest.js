c8b13ca0b3c57163357abe8b21d50b5e6a590032
bringesh-konda <119878875+bringesh-konda@users.noreply.github.com>
2025-10-22 22:13:31 +0530

    findOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((finalName, person) => {
+        let yob = person.yearOfBirth, yod = new Date().getFullYear();
+        if('yearOfDeath' in person) {
+            yod = person.yearOfDeath;
+        }
 
+        let myob = finalName.yearOfBirth, myod = new Date().getFullYear();
+        if('yearOfDeath' in finalName) {
+            myod = finalName.yearOfDeath;
+        }
+
+        if(myod - myob < yod - yob) {
+
+            finalName = person;
+        }
+
+        return finalName;
+    }, people[0]);
 };
 
 // Do not edit below this line
