baa405ddccf2421500123f01b9103df82fa6d4e0
bringesh-konda <119878875+bringesh-konda@users.noreply.github.com>
2025-10-22 16:03:58 +0530

    complete calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+	return arg.reduce((sum, value) => sum += value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arg) {
+  let mul = 1;
+  for(it of arg) {
+    mul *= it;
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fact = 1;
+  for(let i = 1; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
