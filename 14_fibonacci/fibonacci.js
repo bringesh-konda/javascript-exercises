fe719118e4298ea289f965bb486e0dd323c83395
bringesh-konda <119878875+bringesh-konda@users.noreply.github.com>
2025-10-22 21:08:39 +0530

    complete fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if(num < 0)    return "OOPS";
+    if(num == 0)    return 0;
+    let a = 1, b = 1;
+    if(num <= 2) {
+        return a;
+    }
+    for(let i = 0; i < num - 2; i++) {
+        let sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return b;
 };
 
 // Do not edit below this line
